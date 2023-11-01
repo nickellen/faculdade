85bd1681843c1c550decd4e00adf994f4ce09391
monique <98707165+nickellen@users.noreply.github.com>
2023-11-01 20:01:55 -0300

    Update and rename banco_dados/test to banco_dados/av3/source.sql

--- banco_dados/av3/source.sql
+++ banco_dados/av3/source.sql
@@ -3,9 +3,13 @@ DELIMITER $
     -- Esse trigger não está funcionando devido a surgir o seguinte problema:
     -- quando se faz o insert de uma nova ocorrencia com uma chave primária já utilizada, ocorre um erro devido a duplicacão da chave
     -- porém ao tentar deletar esse ocorrência antes da nova insercão, tem-se outro problema:
-    -- de tentar deletar a ocorrência que já está sendo utilizada no trigger.
+    -- voce não pode alterar a tabela (deletar o registro) enquanto o trigger de insert esta acionado
 
-    -- Criar Trigger para inserir row no log antes de inserir uma nova linha na tabela
+    -- Outras abordagem foi pensada: 
+    -- ao verificar que a chave ja existe, bloquear o insert e realizar o update utilizando os novos dados
+    -- Porém não conseguimos encontrar maneira de implementar a ação
+
+    -- Criar Trigger para inserir antigo registro, se já existir, no log antes de inserir o novo na tabela
     CREATE TRIGGER inserir_no_log BEFORE INSERT ON livro FOR EACH ROW
     BEGIN       
         -- obtem o isbn do livro que vai ser inserido
@@ -18,18 +22,18 @@ DELIMITER $
         END IF;
     END$
 
-    -- Criar Trigger para atualizar row 
+    -- Criar Trigger para atualizar row na tabela
     CREATE TRIGGER atualizar_no_log BEFORE UPDATE ON livro FOR EACH ROW
     BEGIN       
-        -- adiciona os valores do log
+        -- adiciona o registro antigo ao log
         INSERT INTO livro_log VALUES(OLD.isbn, OLD.nome, OLD.autor, OLD.genero);
     END$
 
 
-    -- Trigger para excluir da tabela
+    -- Trigger para excluir row da tabela
     CREATE TRIGGER excluir_no_log BEFORE DELETE ON livro FOR EACH ROW
     BEGIN       
-        -- adiciona os valores do log
+        -- adiciona o registro antigo ao log
         INSERT INTO livro_log VALUES(OLD.isbn, OLD.nome, OLD.autor, OLD.genero);
     END$
 
