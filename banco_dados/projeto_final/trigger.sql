e316992bf4ba8f6297f4b9bd4119fd5ccd97afda
monique <98707165+nickellen@users.noreply.github.com>
2023-12-06 14:33:01 -0300

    Update trigger.sql

--- banco_dados/projeto_final/trigger.sql
+++ banco_dados/projeto_final/trigger.sql
@@ -6,7 +6,29 @@ FOR EACH ROW
 BEGIN
 
   UPDATE controle_de_acesso
-     SET qtd_select = qtd_select+1
+     SET qtd_insert = qtd_insert+1
+   WHERE id_conexao = CONNECTION_ID();
+END;
+
+
+CREATE TRIGGER after_delete_trigger
+AFTER DELETE ON livro
+FOR EACH ROW
+BEGIN
+
+  UPDATE controle_de_acesso
+     SET qtd_delete = qtd_delete+1
+   WHERE id_conexao = CONNECTION_ID();
+END;
+
+
+CREATE TRIGGER after_update_trigger
+AFTER UPDATE ON livro
+FOR EACH ROW
+BEGIN
+
+  UPDATE controle_de_acesso
+     SET qtd_update = qtd_update+1
    WHERE id_conexao = CONNECTION_ID();
 END;
 
