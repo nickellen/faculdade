c52534cd14e607c9981402f9c698f3f882fd6687
monique <98707165+nickellen@users.noreply.github.com>
2023-12-06 14:45:02 -0300

    Update cadastrarUsuario.py

--- banco_dados/projeto_final/cadastrarUsuario.py
+++ banco_dados/projeto_final/cadastrarUsuario.py
@@ -1,19 +1,36 @@
+# Autoras: Monique Ellen dos Santos e Fernanda Ribeiro Martins
+# Data: 6 de dezembro de 2023
+
+# Esse script se conecta a um banco de dados MySQL chamado "teste" e cadastra users
+
+# Observações:
+# A execução do script depende da existência prévia do banco de dados "teste" e da tabela "livro".
+# Certifique-se de ter a biblioteca `mysql-connector-python` instalada (`pip install mysql-connector-python`).
+
+
+#!!!!!!!!!!
+# REMOVER COMENTARIOS com !!!! caso for cadastrar usuário para realizar a extratégia de procedures/triggers
+
 import mysql.connector
 import os
 
-#!!!!!!!!!!
-# REMOVER COMENTARIOS caso for cadastrar usuário para realizar a extratégia de procedures/triggers
-
 mydb = mysql.connector.connect(
-  host="localhost",
-  user= "root",
-  password="Iamon_14",
-  database="teste"
+    # Estabelece uma conexão com o banco de dados MySQL, utilizando as credenciais fornecidas (
+    # host, usuário, senha) e selecionando o banco de dados 'teste'.
+
+    host="localhost",
+    user= "root",
+    password="senha",
+    database="teste"
 )
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor()    # Cria um cursor para interagir com o banco de dados.
 
 def getUsers():
+    """
+    Função que recupera e imprime todos os usuários cadastrados 
+    no banco de dados.
+    """
 
     print ("\n=========== Usuario cadastrados =================\n")
 
@@ -25,6 +42,11 @@ def getUsers():
 
 
 def verificarUser(usuario):
+    """
+    Verifica se um usuário já está cadastrado no banco de dados.
+    Retorna True se o usuário já existe, False caso contrário.
+    :param usuario: String com o nome do user
+    """
 
     mycursor.execute("SELECT User FROM mysql.user where Host='%';")
     users =mycursor.fetchall()
@@ -37,6 +59,11 @@ def verificarUser(usuario):
 
 
 def cadastro(mod):
+    """
+    Cadastra um novo usuário no banco de dados. Retorna True se o 
+    cadastro for bem-sucedido, False caso contrário.
+    :param mod: Tipo de usuário (1 para Administrador, 2 para Consulta).    
+    """
 
     usuario = input("Digite o nome do novo usuario: ")
 
@@ -48,13 +75,15 @@ def cadastro(mod):
 
     mycursor.execute("CREATE USER %s@'%' IDENTIFIED BY %s;", (usuario, senha))
     
-    # mycursor.execute("GRANT EXECUTE ON PROCEDURE teste.login_trigger TO %s@'%';", (usuario,)) 
+    # !!!! mycursor.execute("GRANT EXECUTE ON PROCEDURE teste.login_trigger TO %s@'%';", (usuario,))
     mycursor.execute("GRANT UPDATE, SELECT, INSERT ON teste.controle_de_acesso TO %s@'%';", (usuario,))
     mycursor.execute("FLUSH PRIVILEGES;")
 
     if mod==1:
 
         mycursor.execute("GRANT UPDATE, INSERT, DELETE, SELECT ON teste.livro TO %s@'%';", (usuario,))
+        mycursor.execute("GRANT INSERT, SELECT ON teste.livro_log TO %s@'%';", (usuario,))
+
     else:
 
         mycursor.execute("GRANT SELECT ON teste.livro TO %s@'%';", (usuario,))
@@ -62,6 +91,10 @@ def cadastro(mod):
     return True
 
 def relatorio():
+    """
+    Exibe um relatório de todos os usuários cadastrados no 
+    banco de dados.
+    """
 
     os.system("clear")
 
@@ -89,4 +122,6 @@ def relatorio():
     
     print ("\n==================================================")
 
+
+# Chamada da função para exibir o relatório
 relatorio()
